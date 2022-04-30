8d80436d4fad04c8f048eaecd8ec96ab0cd6b6c7
shasap <sapan.shah.qa@gmail.com>
2022-04-30 20:27:08 +0100

    addition

--- src/FirstProject.java
+++ src/FirstProject.java
@@ -1,7 +1,12 @@
 public class FirstProject {
 
+    public static void addition (int a, int b){
+        System.out.println("Addition of "+a+ " & "+b+" = " +(a+b));
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+        addition(20,10);
 
     }
 }
