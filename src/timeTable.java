8b74b5d1d621d993b0198428be5dca2259f09c3b
shasap <sapan.shah.qa@gmail.com>
2022-04-30 23:05:30 +0100

    Time table of 2 using while loop

---
+++ src/timeTable.java
@@ -0,0 +1,11 @@
+public class timeTable {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i<=10)
+        {
+            System.out.println("2 x "+i+" = "+(2*i));
+            i++;
+        }
+    }
+}
